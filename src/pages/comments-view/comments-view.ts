3e83f74c64ea9599e885cd0420ce1ab4f936d877
j-west <westja89@gmail.com>
2017-02-27 10:03:07 -0600

    Update comments page and ts file

--- src/pages/comments-view/comments-view.ts
+++ src/pages/comments-view/comments-view.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AuthService } from '../../providers/auth-service';
+
 
 /*
   Generated class for the CommentsView page.
@@ -16,8 +18,11 @@ export class CommentsViewPage {
 
   post: any;
   comments: FirebaseListObservable<any>;
+  currentUser: any;
+  comment: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public af: AngularFire) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private _auth: AuthService, public af: AngularFire) {
+    this.currentUser = this._auth.authState.auth;
     this.post = navParams.get('beer');
     this.comments = af.database.list(`beers/${this.post.$key}/comments`)
   }
@@ -26,6 +31,16 @@ export class CommentsViewPage {
     this.viewCtrl.dismiss();
   }
 
+  addComment(comment) {
+    let newComment = {
+      'comment' : comment,
+      'user' : this.currentUser.displayName,
+      'userImg' : this.currentUser.photoURL
+    };
+
+    this.comments.push(newComment);
+    this.comment = '';
+  }
 
 
 
